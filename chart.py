6108311a9a36345ffbc3381d02853ba1788a071a
prepda-com <prepda@web.de>
2026-05-24 01:44:44 +0200

    Echte Apple-Aktiendaten hinzugefügt

--- chart.py
+++ chart.py
@@ -2,15 +2,11 @@ import plotly.graph_objects as go
 import yfinance as yf
 
 # 1. Echte Aktiendaten abrufen
-# 'AAPL' steht für Apple. Für Tesla nimmst du 'TSLA', für Bitcoin 'BTC-USD'
 ticker_symbol = 'AAPL' 
 ticker_data = yf.Ticker(ticker_symbol)
 
-# Wir holen die historischen Daten (period='6mo' bedeutet die letzten 6 Monate)
-# '1d' bedeutet eine Kerze pro Tag
+# Wir holen die historischen Daten der letzten 6 Monate
 df = ticker_data.history(period='6mo', interval='1d')
-
-# Wir müssen den Index (das Datum) wieder als normale Spalte verfügbar machen
 df = df.reset_index()
 
 # 2. Den interaktiven Candlestick-Chart erstellen
@@ -26,8 +22,8 @@ fig = go.Figure(data=[go.Candlestick(
 fig.update_layout(
     title=f'Echter Live-Chart: {ticker_symbol} (Letzte 6 Monate)',
     yaxis_title='Kurs in USD',
-    xaxis_rangeslider_visible=False, # Entfernt den Schieberegler unten für mehr Platz
-    template='plotly_dark'           # Schicker Dark Mode
+    xaxis_rangeslider_visible=False,
+    template='plotly_dark'
 )
 
 # 4. Als index.html für deine Webseite speichern
